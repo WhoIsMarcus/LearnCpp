8d2bfba5ee164ee8b9a6dde5b8b5e580a26806fd
Marcus <marcusmccrea29@gmail.com>
2026-06-25 18:12:11 -0600

    matrix multiply working, i think?

--- BasicMatrix/tensoradvanced.cpp
+++ BasicMatrix/tensoradvanced.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
 #include <vector>
 
-
 using namespace std;
 
 struct Point {
     vector<float> coords;
+
+    Point(const vector<float>& c) : coords(c) {}
 };
-
-
 
 struct Tensor {
     vector<float> data;
-    int rows;
-    int cols;
+    size_t rows;
+    size_t cols;
 
-    //Runs when Tensor is created
-    Tensor(vector<vector<float>> matrix_2d){
+    //From 2D vector
+    Tensor(const vector<vector<float>>& matrix_2d) {
         rows = matrix_2d.size();
-        cols = rows > 0 ? matrix_2d[0].size() : 0; // condition ? value_ifTrue : value_ifFalse
-        // Flatten 2d to 1d
+        cols = rows > 0 ? matrix_2d[0].size() : 0;
+
         for (const auto& row : matrix_2d) {
             for (float value : row) {
                 data.push_back(value);
@@ -27,63 +26,57 @@ struct Tensor {
         }
     }
 
-    float get(int r, int c) const{
-        int index = (r * cols) + c;
-        return data[index];
+    //Blank tensor
+    Tensor(size_t r, size_t c) {
+        rows = r;
+        cols = c;
+        data.resize(r * c, 0.0f);
     }
 
+    float get(size_t r, size_t c) const {
+        return data[r * cols + c];
+    }
 
+    void set(size_t r, size_t c, float val) {
+        data[r * cols + c] = val;
+    }
 
     void print() const {
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
+        for (size_t r = 0; r < rows; r++) {
+            for (size_t c = 0; c < cols; c++) {
                 cout << get(r, c) << " ";
             }
             cout << endl;
         }
     }
 
-
-    vector<float> getRow(int r) const {
+    vector<float> getRow(size_t r) const {
         vector<float> row;
-        for (int c = 0; c < cols; c++) {
+        for (size_t c = 0; c < cols; c++) {
             row.push_back(get(r, c));
         }
         return row;
     }
 
-    vector<float> getCol(int c) const {
+    vector<float> getCol(size_t c) const {
         vector<float> col;
-        for (int r = 0; r < rows; r++) {
+        for (size_t r = 0; r < rows; r++) {
             col.push_back(get(r, c));
         }
         return col;
     }
-
-    Tensor(int r, int c) { //Blank tensor
-        rows = r;
-        cols = c;
-        data.resize(r * c, 0.0f);
-    }
-
-    void set(int r, int c, float val) { //set items to value
-        data[r * cols + c] = val;
-    }
-
-
 };
 
+float dot(const Point& a, const Point& b) {
+    float sum = 0.0f;
+    size_t n = min(a.coords.size(), b.coords.size());
 
+    for (size_t i = 0; i < n; i++) {
+        sum += a.coords[i] * b.coords[i];
+    }
 
-float dot(const Point& a, const Point& b){
-    float final = 0.0f;
-    for (size_t i = 0; i < a.coords.size(); i++){
-        final += a.coords[i] * b.coords[i];
-    };
-    return final;
+    return sum;
 }
-
-
 
 Tensor multiply(const Tensor& a, const Tensor& b) {
 
@@ -98,7 +91,9 @@ Tensor multiply(const Tensor& a, const Tensor& b) {
             vector<float> colB = b.getCol(j);
 
             float sum = 0.0f;
-            for (size_t k = 0; k < rowA.size(); k++) {
+            size_t n = min(rowA.size(), colB.size());
+
+            for (size_t k = 0; k < n; k++) {
                 sum += rowA[k] * colB[k];
             }
 
@@ -108,9 +103,6 @@ Tensor multiply(const Tensor& a, const Tensor& b) {
 
     return result;
 }
-
-
-
 
 int main() {
 
@@ -122,11 +114,7 @@ int main() {
 
     Tensor finalmult = multiply(matrix_a, matrix_b);
 
-    Tensor My_tensor({{1, 2}, {3, 4}});
-
-    cout << "Dot:" << dot(a, b) << "\n";
-    cout << "Matrix A*B: \n";
+    cout << "Dot: " << dot(a, b) << "\n";
+    cout << "Matrix A * B:\n";
     finalmult.print();
-
-
 }
